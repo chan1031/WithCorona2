d293df5dddd6fcc023daaf1efe46b305afc4f7f2
chan1031 <dragonlsc@naver.com>
2020-11-18 13:48:32 +0900

    - 사장님 로그인 안되는 오류 수정
    - 사장 회원가입시 아이디 체크 안되는 오류 수정

--- app/src/main/java/com/cookandroid/myapplication/login/bossLogin/BossLoginRequest.java
+++ app/src/main/java/com/cookandroid/myapplication/login/bossLogin/BossLoginRequest.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class BossLoginRequest extends StringRequest {
 
     // 서버 URL 설정 (PHP 파일 연동)
-    final static private String URL = "http://49.172.168.109:1228/NfcInsert.php";
+    final static private String URL = "http://49.172.168.109:1228/BossLogin.php";
     private Map<String, String> map;
 
     public BossLoginRequest(String userID, String userPassword, Response.Listener<String> listener){
